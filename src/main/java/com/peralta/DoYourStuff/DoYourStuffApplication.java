ab97c27f752df2c21122664c3f9f9adacb580c61
PedroFPeralta <200221137@estudantes.ips.pt>
2023-12-17 21:18:07 +0000

    Init

---
+++ src/main/java/com/peralta/DoYourStuff/DoYourStuffApplication.java
@@ -0,0 +1,13 @@
+package com.peralta.DoYourStuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoYourStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoYourStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peralta/DoYourStuff/DoYourStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peralta.DoYourStuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoYourStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
